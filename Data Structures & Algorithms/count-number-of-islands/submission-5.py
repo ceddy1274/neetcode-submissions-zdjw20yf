229f7cd97f5e2bc72513868b99dd1e5debf75963
Cameron Eddy <132533842+ceddy1274@users.noreply.github.com>
2026-07-03 01:10:31 -0500

    Add: count-number-of-islands - submission-5

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        visited = {}
+        def bfs(i, j):
+            if i >= 0 and i < len(grid) and j >= 0 and j < len(grid[i]) and grid[i][j] == '1': 
+                    if (i,j) not in visited:
+                        nonlocal count
+                        visited[(i,j)] = True
+                        count += 1
+                    visitNeighbors(i,j-1)
+                    visitNeighbors(i,j+1)
+                    visitNeighbors(i-1,j)
+                    visitNeighbors(i+1,j)
+
+        def visitNeighbors(i,j):
+            if (i,j) not in visited:
+                visited[(i,j)] = True
+                bfs(i,j)
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                bfs(i, j)
+        
+        print(visited)
+        return count
+        
+
+        
